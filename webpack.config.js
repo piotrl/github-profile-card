d2a3cfee68adcdba9e51fbfaf073b48b5d04ca3b
Piotr Lewandowski <grovman@gmail.com>
2019-03-18 23:17:02 +0100

    chore(build): Build production and dev bundles

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 const PACKAGE = require('./package.json');
+
+const ENV = process.env.WEBPACK_ENV;
+const libraryName = 'gh-profile-card';
 const banner =
 `
 ${PACKAGE.name} - ${PACKAGE.version} | ${PACKAGE.license}
@@ -11,7 +14,7 @@ ${PACKAGE.name} - ${PACKAGE.version} | ${PACKAGE.license}
 module.exports = {
     entry: './src/gh-widget-init.ts',
     output: {
-        filename: 'gh-profile-card.min.js',
+        filename: `${libraryName}.min.js`,
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
@@ -19,7 +22,7 @@ module.exports = {
         new webpack.BannerPlugin(banner),
     ],
     mode: 'production',
-    devtool: 'inline-source-map',
+    devtool: ENV === 'dev' ? 'inline-source-map' : false,
     module: {
         rules: [
             {
