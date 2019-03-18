47de36bfe00ddcccc9e13cb9833c5ab5f8fa2d5a
Piotr Lewandowski <piotr.lewandowski@dynatrace.com>
2019-03-18 20:10:47 +0100

    #1 chore(test): Set-up jest configuration

---
+++ src/gh-dom-operator.spec.ts
@@ -0,0 +1,11 @@
+describe('DomOperator', () => {
+    it('should compile', () => {
+        // given
+
+        // when
+        const result = true;
+
+        // then
+        expect(result).toBe(true);
+    });
+});
