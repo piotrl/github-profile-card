e7ad5724400a2957de29f693fba82623e4358279
Piotr Lewandowski <grovman@gmail.com>
2020-01-20 21:56:51 +0100

    refactor(demo): use 2-spaced tabs

--- demo/demo.js
+++ demo/demo.js
@@ -1,99 +1,99 @@
 (function(GitHubCard, widgetGenerator) {
-    'use strict';
+  'use strict';
 
-    // Generating new widget from user input
-    document.addEventListener('DOMContentLoaded', () => {
-        const options = {
-            template: '#github-card-demo',
-            sortBy: 'stars', // possible: 'stars', 'updateTime'
-            headerText: 'Most starred repositories',
-            maxRepos: 5
-        };
-        overrideOptionsByUrlParams(options);
+  // Generating new widget from user input
+  document.addEventListener('DOMContentLoaded', () => {
+    const options = {
+      template: '#github-card-demo',
+      sortBy: 'stars', // possible: 'stars', 'updateTime'
+      headerText: 'Most starred repositories',
+      maxRepos: 5
+    };
+    overrideOptionsByUrlParams(options);
 
-        let widget = new GitHubCard(options);
-        widget.init();
-        refreshConfigTextarea(options);
+    let widget = new GitHubCard(options);
+    widget.init();
+    refreshConfigTextarea(options);
 
-        initSortingControl(options, refreshWidget);
-        initRepositoriesControl(options, refreshWidget);
-        initUserControl(options, initWidget);
+    initSortingControl(options, refreshWidget);
+    initRepositoriesControl(options, refreshWidget);
+    initUserControl(options, initWidget);
 
-        function initWidget(options) {
-            widget = new GitHubCard(options);
-            widget.init();
-            refreshConfigTextarea(options);
-        }
+    function initWidget(options) {
+      widget = new GitHubCard(options);
+      widget.init();
+      refreshConfigTextarea(options);
+    }
 
-        function refreshWidget(updatedOptions) {
-            widget.refresh(updatedOptions);
-            refreshConfigTextarea(updatedOptions);
-        }
+    function refreshWidget(updatedOptions) {
+      widget.refresh(updatedOptions);
+      refreshConfigTextarea(updatedOptions);
+    }
+  });
+
+  function refreshConfigTextarea(updatedOptions) {
+    const textarea = document.getElementById('install-code');
+    textarea.value = widgetGenerator.regenerate(updatedOptions);
+  }
+
+  // Sort repository acording to
+  // radio inputs on website
+  function initSortingControl(options, refreshWidget) {
+    var $sortingRadios = document.querySelectorAll(
+      '.choose-repo-sorting label'
+    );
+
+    // sort by update time
+    $sortingRadios[0].addEventListener('click', event => {
+      event.target.classList.add('active');
+      $sortingRadios[1].classList.remove('active');
+
+      options.sortBy = 'updateTime';
+      options.headerText = event.target.textContent + ' repositories';
+
+      refreshWidget(options);
     });
 
-    function refreshConfigTextarea(updatedOptions) {
-        const textarea = document.getElementById('install-code');
-        textarea.value = widgetGenerator.regenerate(updatedOptions);
+    // sort by starrgazers
+    $sortingRadios[1].addEventListener('click', event => {
+      event.target.classList.add('active');
+      $sortingRadios[0].classList.remove('active');
+
+      options.sortBy = 'stars';
+      options.headerText = event.target.textContent + ' repositories';
+
+      refreshWidget(options);
+    });
+  }
+
+  // Manipulating the number of repositories
+  function initRepositoriesControl(options, refreshWidget) {
+    const $inputNumber = document.getElementById('gh-reposNum');
+
+    $inputNumber.onchange = () => {
+      options.maxRepos = $inputNumber.value;
+      refreshWidget(options);
+    };
+  }
+
+  // Creating brand new widget instance
+  // for user that we type in input
+  function initUserControl(options, cb) {
+    const $input = document.getElementById('gh-uname');
+    const $submit = document.getElementById('gh-uname-submit');
+
+    $submit.addEventListener('click', event => {
+      options.username = $input.value;
+      cb(options);
+
+      event.preventDefault();
+    });
+  }
+
+  function overrideOptionsByUrlParams(options) {
+    const queryParameters = new URL(document.location).searchParams;
+    for (const [key, value] of queryParameters) {
+      options[key] = value;
     }
-
-    // Sort repository acording to
-    // radio inputs on website
-    function initSortingControl(options, refreshWidget) {
-        var $sortingRadios = document.querySelectorAll(
-            '.choose-repo-sorting label'
-        );
-
-        // sort by update time
-        $sortingRadios[0].addEventListener('click', event => {
-            event.target.classList.add('active');
-            $sortingRadios[1].classList.remove('active');
-
-            options.sortBy = 'updateTime';
-            options.headerText = event.target.textContent + ' repositories';
-
-            refreshWidget(options);
-        });
-
-        // sort by starrgazers
-        $sortingRadios[1].addEventListener('click', event => {
-            event.target.classList.add('active');
-            $sortingRadios[0].classList.remove('active');
-
-            options.sortBy = 'stars';
-            options.headerText = event.target.textContent + ' repositories';
-
-            refreshWidget(options);
-        });
-    }
-
-    // Manipulating the number of repositories
-    function initRepositoriesControl(options, refreshWidget) {
-        const $inputNumber = document.getElementById('gh-reposNum');
-
-        $inputNumber.onchange = () => {
-            options.maxRepos = $inputNumber.value;
-            refreshWidget(options);
-        };
-    }
-
-    // Creating brand new widget instance
-    // for user that we type in input
-    function initUserControl(options, cb) {
-        const $input = document.getElementById('gh-uname');
-        const $submit = document.getElementById('gh-uname-submit');
-
-        $submit.addEventListener('click', event => {
-            options.username = $input.value;
-            cb(options);
-
-            event.preventDefault();
-        });
-    }
-
-    function overrideOptionsByUrlParams(options) {
-        const queryParameters = new URL(document.location).searchParams;
-        for (const [key, value] of queryParameters) {
-            options[key] = value;
-        }
-    }
+  }
 })(window.GitHubCard, window.widgetGenerator);
--- demo/widget-generator.js
+++ demo/widget-generator.js
@@ -1,29 +1,29 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    const attributes = {
-        username: 'data-username',
-        maxRepos: 'data-max-repos',
-        sortBy: 'data-sort-by',
-        headerText: 'data-header-text'
-    };
+  const attributes = {
+    username: 'data-username',
+    maxRepos: 'data-max-repos',
+    sortBy: 'data-sort-by',
+    headerText: 'data-header-text'
+  };
 
-    window.widgetGenerator = {
-        regenerate: regenerate
-    };
+  window.widgetGenerator = {
+    regenerate: regenerate
+  };
 
-    function regenerate(options) {
-        const attributesTemplate = Object.keys(options)
-            .map(option => {
-                const attribute = attributes[option];
-                const value = options[option];
-                if (!attribute) {
-                    return '';
-                }
-                return `\n\t${attribute}="${value}"`;
-            })
-            .join('');
+  function regenerate(options) {
+    const attributesTemplate = Object.keys(options)
+      .map(option => {
+        const attribute = attributes[option];
+        const value = options[option];
+        if (!attribute) {
+          return '';
+        }
+        return `\n\t${attribute}="${value}"`;
+      })
+      .join('');
 
-        return `<div id="github-card" ${attributesTemplate}>\n</div>`;
-    }
+    return `<div id="github-card" ${attributesTemplate}>\n</div>`;
+  }
 })();
