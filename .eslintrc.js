2ee35812de5a61032fe80f48c9ac86b682df3db2
Piotr Lewandowski <grovman@gmail.com>
2020-01-20 23:16:17 +0100

    feat(lint): configure additional eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,13 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'jest',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:jest/recommended',
   ],
 };
